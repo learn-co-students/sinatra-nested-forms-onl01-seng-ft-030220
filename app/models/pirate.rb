5b2b4962df76c0d4ee8f6fffc6cbc220fc245ff9
matt_kuras <mattkuras@gmail.com>
2020-04-15 17:17:07 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+
+    @@all = []
+
+    def initialize(params)
+        @name = name 
+        @weight = weight 
+        @height = height 
+        @@all << self 
+    end
+
+    attr_accessor :name, :weight, :height
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+    attr_accessor :name, :type, :booty_attributes
+    @@all = []
+    def initialize(params)
+        @name = name 
+        @type = type 
+        @booty = booty
+        @@all << self 
+    end
+    def self.all 
+        @@all 
+    end
+    def self.clear 
+        @@all = []
+    end
 end
